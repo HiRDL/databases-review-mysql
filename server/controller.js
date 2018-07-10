dc4c496d237594759af9353a9f1537b7be95cfa7
elaskowski <evalaskows@gmail.com>
2018-07-10 13:37:20 -0700

    Intial skeleton

--- server/controller.js
+++ server/controller.js
@@ -1,34 +1,15 @@
-const db = require('../database-mysql/models');
-
 module.exports = {
   fetch: (req, res) => {
     // http://localhost:3000/api/todolist/?name=To+Do+List
     const { listName } = req.query;
-    db.todo.findAll({
-      where: { list_name: listName }
-    })
-    .then(todos => {
-      if (todos) { res.status(200).send(todos) }
-      else { res.status(404).send('List not found'); }
-    });
+    res.status(200).send('fetched');
   },
   post: (req, res) => {
     const { todo, listName } = req.body;
-    db.todo.create({
-        name: todo,
-        list_name: listName
-    })
-    .then(todo => {
-      res.status(201).send(todo);
-    });
+    res.status(201).send('posted');
   },
   delete: (req, res) => {
     const { todo } = req.query;
-    db.todo.destroy({
-      where: { name: todo }
-    })
-    .then(todos => {
-      res.status(202).send('Entry deleted');
-    })
+    res.status(202).send('deleted');
   }
 }
